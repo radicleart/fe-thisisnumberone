fac14fe7e3d3390986937a02fdb12f94891b3afd
Thomas Lefebvre <thomas.lefebvre@student-cs.fr>
2021-04-01 15:23:31 +0200

    Creation of the footer (see : https://dbid.prismic.io/documents~b=working&c=published&l=en-gb/YGSFJhEAACQAJ0K9/)

--- src/components/layout/MainFooter.vue
+++ src/components/layout/MainFooter.vue
@@ -1,148 +1,75 @@
 <template>
-<div v-if="content">
-  <footer class="footer-container">
-    <div style="height: 240px;" class="row">
-      <div class="col-12 text-center mt-4"><h1>This Is Definitely Not The One</h1></div>
-      <div class="col-12 text-center">By Risidio</div>
-      <div class="col-12 text-center social-icons">
-        <span style="font-size: 15px;">
-          <a href="https://discord.gg/qvuxB9P"><b-icon class="mx-4" scale="3" icon="discord"></b-icon></a>
-          <a href="https://twitter.com/Risidio1"><b-icon class="mx-4" scale="3"  icon="twitter"></b-icon></a>
-          <a href="https://www.instagram.com/risidio_official/"><b-icon class="mx-4" scale="3"  icon="instagram"></b-icon></a>
-        </span>
+  <div>
+
+    <div class="title">
+      <h1>{{ content.title[0].text }}</h1>
+    </div>
+
+    <div class="row">
+      <div class="col">
+        <div v-for="(item,index) of content.column_about" :key="index">
+          <div><a :href=item.link.url>{{ item.title_of_the_link[0].text }}</a></div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div v-for="(item,index) of content.column__1" :key="index">
+          <div><a :href=item.link.url>{{ item.title_of_the_link[0].text }}</a></div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div v-for="(item,index) of content.news_column" :key="index">
+          <div><a :href=item.link.url>{{ item.title_of_the_link[0].text }}</a></div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div v-for="(item,index) of content.partners_column" :key="index">
+          <div><a :href=item.link.url>{{ item.title_of_the_link[0].text }}</a></div>
+        </div>
       </div>
     </div>
-    <div class="row text-center">
-      <div class="col-12">
-        &copy; &nbsp; {{ year }} Risidio Ltd. All right reserved.
+
+    <div class="bottom">
+      <div class="left corner">
+        {{ content.left_bottom_corner[0].text }}
       </div>
+
+      <div class="right corner">
+        {{ content.right_bottom_corner[0].text }}
+      </div>
+
     </div>
-  </footer>
-</div>
+  </div>
 </template>
+
 <script>
+
 export default {
-  props: {
-    backgroundColor: String,
-    type: String
-  },
+  name: 'MainFooter',
   data () {
     return {
-      year: new Date().getFullYear()
+      content: {}
     }
   },
   methods: {
-    getContent (contentType) {
-      const content = this.$store.getters['contentStore/getHomepage']
-      if (content) {
-        let sitecontent = ''
-        content.footer[0][contentType].forEach(function (option) {
-          sitecontent += option.text
+    getContent () {
+      this.$prismic.client.getSingle('mainfooter')
+        .then((document) => {
+          this.content = document.data
+          console.log(this.content)
         })
-        return sitecontent
-      }
-      return {}
     }
   },
-  computed: {
-    content () {
-      const content = this.$store.getters['contentStore/getHomepage']
-      return content
-    }
+  created () {
+    this.getContent()
   }
 }
 </script>
+
 <style>
-.footer-container {
-  height: 280px;
-}
-
-.footer-container h2 {
-  font-size: 17px;
-  font-weight: 200;
-}
-
-.footer-container p {
-  font-size: 12px;font-weight: 700; padding: 5px 0 0 0; margin: 0;
-}
-
-.footer-logo {
-  width: 255px;
-  height: 100px;
-}
-
-/* FOOTER ON MOBILE */
-.mobile-footer p {
-  width: 30%;
-  margin: 0 auto !important;
-  text-align: center;
-  font-size: 10px !important;
-  font-weight: 400 !important;
-  letter-spacing: 0.5px;
-  padding-top: 8px !important;
-}
-
-.mobile-footer h2 {
-  font-size: 15px !important;
-  letter-spacing: 0.75px;
-  margin-bottom: 2px;
-}
-
-.mobile-footer--contact p:last-child, .mobile-footer--contact p:nth-child(3) {
-  display: none;
-}
-
-.social-icons {
-  margin-top: 11px;
-}
-
-.social-icons a:not(:last-child) {
-  margin-right: 15px;
-}
-
-.social-icons a {
-  color: #fff;
-}
-
-.social-icons a:hover {
-  color: #fff;
-}
-
-.mobile-footer--copyright {
-  font-size: 8px;
-  font-weight: 300;
-  padding-bottom: 22px;
-}
-
-@media only screen and (max-width: 991px) {
-  .footer-logo {
-    width: 138px;
-    height: 55px;
-  }
-}
-
-@media only screen and (max-width: 768px) {
-  .content-footer {
-    margin-left: 3rem;
-  }
-}
-
-@media only screen and (max-width: 575px) {
-  .footer-logo {
-    width: 200px;
-    height: auto;
-  }
-  .footer-container {
-    height: 395px;
-    display: flex;
-    flex-flow: column;
-    justify-content: space-between;
-  }
-}
-
-@media only screen and (max-width: 320px) {
-  .mobile-footer p {
-    width: 33%;
-  }
+body {
+  background-color: white;
 }
 </style>
