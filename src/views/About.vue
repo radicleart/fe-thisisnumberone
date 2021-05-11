cc440ccda4b4e3ea068124df722a0a59f47d25b0
Thomas Lefebvre <thomas.lefebvre@student-cs.fr>
2021-05-11 16:18:58 +0200

    Creating the evironment section

--- src/views/About.vue
+++ src/views/About.vue
@@ -22,6 +22,7 @@
   <about-section2 :content="content"></about-section2>
   <about-section3 :content="content"></about-section3>
   <about-section4 :content="content"></about-section4>
+  <about-section5 :content="content"></about-section5>
   <!-- <about-section6 :content="content"></about-section6> -->
 </div>
 </template>
@@ -31,7 +32,7 @@ import AboutSection1 from '@/components/about/AboutSection1'
 import AboutSection2 from '@/components/about/AboutSection2'
 import AboutSection3 from '@/components/about/AboutSection3'
 import AboutSection4 from '@/components/about/AboutSection4'
-// import AboutSection5 from '@/components/about/AboutSection5'
+import AboutSection5 from '@/components/about/AboutSection5'
 // import AboutSection6 from '@/components/about/AboutSection6'
 import { APP_CONSTANTS } from '@/app-constants'
 
@@ -41,8 +42,8 @@ export default {
     AboutSection1,
     AboutSection2,
     AboutSection3,
-    AboutSection4
-    // AboutSection5
+    AboutSection4,
+    AboutSection5
     // AboutSection6
   },
   data () {
