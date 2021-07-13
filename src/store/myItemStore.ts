9335eb5f5a36f1cd8eed3dc90e1e82d7677c4581
Mike Cohen <mjoecohen@gmail.com>
2021-07-13 07:18:42 +0100

    fix to gaia data

--- src/store/myItemStore.ts
+++ src/store/myItemStore.ts
@@ -260,7 +260,6 @@ const myItemStore = {
         if (item.attributes.artworkClip && item.attributes.artworkClip.dataUrl) item.attributes.artworkClip.dataUrl = null
         if (item.attributes.artworkFile && item.attributes.artworkFile.dataUrl) item.attributes.artworkFile.dataUrl = null
         if (item.attributes.coverImage && item.attributes.coverImage.dataUrl) item.attributes.coverImage.dataUrl = null
-        item.attributes = item.attributes
         if (!item.metaDataUrl && !profile.gaiaHubConfig) {
           throw new Error('profile needs to refresh - please reload current page..')
         }
